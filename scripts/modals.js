72b02d6f3f6dc7f700633c18fec3e931bee9cff6
Danny Tran <dannytran3@gmail.com>
2021-05-23 21:53:08 -0400

    finished modal

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,8 +1,15 @@
 export const modal = () => {
     document.querySelectorAll('.fui-modal-trigger').forEach((trigger) => {
-        trigger.addEventListener('click', (e) => {
+        trigger.addEventListener('click', e => {
             const target = e.currentTarget.getAttribute('data-modal-target');
             document.querySelector(`#${target}.fui-modal__background`).classList.toggle('open');
         });
+
     });
+
+    document.querySelectorAll(`.close`).forEach(button => {
+        button.addEventListener('click', e => {
+            e.currentTarget.closest('.fui-modal__background').classList.toggle('open');
+        })
+    })
 };
